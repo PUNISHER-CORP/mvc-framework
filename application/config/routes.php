0f0ec23858e209d8af20f5f83eb33c7572aadbb3
Leonid Dobryvechir <leo.dobrivecher@gmail.com>
2018-08-01 23:20:50 +0300

    add route

---
+++ application/config/routes.php
@@ -0,0 +1,16 @@
+<?php
+
+return 
+[
+    'account/login' => 
+    [
+        'controller' => 'test',
+        'action' => 'login',
+    ],
+
+    'news/show' => 
+    [
+        'controller' => 'news',
+        'action' => 'show',
+    ],
+];
--- application/core/Router.php
+++ application/core/Router.php
@@ -10,17 +10,23 @@ class Router
 
     public function __construct() 
     {
-        //
+        $arr = require 'application/config/routes.php';
+        //debug($arr);
+        foreach($arr as $key => $val)
+        {
+            $this->add($key, $val);
+        }
     }
 
-    public function add()
+    public function add($route, $params)
     {
-        //
+        $route = '#^'.$route.'$#';
+        $this->routes[$route] = $params;
     }
 
     public function match()
     {
-        //
+        $url = $_SERVER['REQUEST_URI'];
     }
 
     public function run()
